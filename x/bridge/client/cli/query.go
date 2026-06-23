0d9f11430b892ddeaf2ef457c09e1fd0812e54a2
Liviu Epure <liviu.etty@gmail.com>
2026-06-24 02:57:29 +0300

    bridge: real proto-generated query service + CLI (core)
    Fourth module of the query proto-gap rollout. Added a gRPC Query service:
    query.proto (Config, ChainConfig(s), Validator(s), Operation(s) + views); real
    CLI (config/chains/chain/validators/operations/operation). The proto query
    server is in the proprietary keeper as a distinct type from the internal helper.

--- x/bridge/client/cli/query.go
+++ x/bridge/client/cli/query.go
@@ -4,6 +4,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 
 	"github.com/qorechain/qorechain-core/x/bridge/types"
 )
@@ -16,6 +17,139 @@ func GetQueryCmd() *cobra.Command {
 		SuggestionsMinimumDistance: 2,
 		RunE:                       client.ValidateCmd,
 	}
+	cmd.AddCommand(
+		cmdConfig(),
+		cmdChains(),
+		cmdChain(),
+		cmdValidators(),
+		cmdOperations(),
+		cmdOperation(),
+	)
+	return cmd
+}
 
+func cmdConfig() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "config",
+		Short: "Show the global bridge config",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			res, err := types.NewQueryClient(clientCtx).Config(cmd.Context(), &types.QueryConfigRequest{})
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+func cmdChains() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "chains",
+		Short: "List all configured chains",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			res, err := types.NewQueryClient(clientCtx).ChainConfigs(cmd.Context(), &types.QueryChainConfigsRequest{})
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+func cmdChain() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "chain [chain-id]",
+		Short: "Show a single chain config",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			res, err := types.NewQueryClient(clientCtx).ChainConfig(cmd.Context(), &types.QueryChainConfigRequest{ChainId: args[0]})
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+func cmdValidators() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "validators",
+		Short: "List all bridge validators",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			res, err := types.NewQueryClient(clientCtx).Validators(cmd.Context(), &types.QueryValidatorsRequest{})
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+func cmdOperations() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "operations",
+		Short: "List all bridge operations",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			res, err := types.NewQueryClient(clientCtx).Operations(cmd.Context(), &types.QueryOperationsRequest{})
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+func cmdOperation() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "operation [id]",
+		Short: "Show a single bridge operation",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			res, err := types.NewQueryClient(clientCtx).Operation(cmd.Context(), &types.QueryOperationRequest{Id: args[0]})
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
 	return cmd
 }
