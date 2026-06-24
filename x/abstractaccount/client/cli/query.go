95bf75d99a5a8da1305004ea789b3ff83690fb59
Liviu Epure <liviu.etty@gmail.com>
2026-06-24 03:03:20 +0300

    abstractaccount: real proto-generated query service + CLI (core)
    Sixth module of the query proto-gap rollout. query.proto (Config, Account,
    Accounts + views); real CLI (config/account/accounts).

--- x/abstractaccount/client/cli/query.go
+++ x/abstractaccount/client/cli/query.go
@@ -4,6 +4,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 
 	"github.com/qorechain/qorechain-core/x/abstractaccount/types"
 )
@@ -16,34 +17,37 @@ func GetQueryCmd() *cobra.Command {
 		SuggestionsMinimumDistance: 2,
 		RunE:                       client.ValidateCmd,
 	}
-
 	cmd.AddCommand(
 		CmdQueryConfig(),
 		CmdQueryAccount(),
+		CmdQueryAccounts(),
 	)
-
 	return cmd
 }
 
-// CmdQueryConfig returns the command to query the abstract account config.
+// CmdQueryConfig queries the abstract account config.
 func CmdQueryConfig() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "config",
 		Short: "Query the abstract account configuration",
 		Args:  cobra.NoArgs,
-		RunE: func(cmd *cobra.Command, args []string) error {
+		RunE: func(cmd *cobra.Command, _ []string) error {
 			clientCtx, err := client.GetClientQueryContext(cmd)
 			if err != nil {
 				return err
 			}
-			_ = clientCtx
-			return cmd.Help()
+			res, err := types.NewQueryClient(clientCtx).Config(cmd.Context(), &types.QueryConfigRequest{})
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
 		},
 	}
+	flags.AddQueryFlagsToCmd(cmd)
 	return cmd
 }
 
-// CmdQueryAccount returns the command to query an abstract account.
+// CmdQueryAccount queries an abstract account by address.
 func CmdQueryAccount() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "account [address]",
@@ -54,10 +58,35 @@ func CmdQueryAccount() *cobra.Command {
 			if err != nil {
 				return err
 			}
-			_ = clientCtx
-			_ = args[0]
-			return cmd.Help()
+			res, err := types.NewQueryClient(clientCtx).Account(cmd.Context(), &types.QueryAccountRequest{Address: args[0]})
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
 		},
 	}
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// CmdQueryAccounts lists all abstract accounts.
+func CmdQueryAccounts() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "accounts",
+		Short: "List all abstract accounts",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			res, err := types.NewQueryClient(clientCtx).Accounts(cmd.Context(), &types.QueryAccountsRequest{})
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
 	return cmd
 }
