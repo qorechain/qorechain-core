161777b1d17cc5f9dab29cd4192851bd6a6c1971
Liviu Epure <liviu.etty@gmail.com>
2026-06-24 03:00:53 +0300

    multilayer: real proto-generated query service + CLI (core)
    Fifth module of the query proto-gap rollout. query.proto (Params, Layer, Layers,
    RoutingStats + views); real CLI (params/layer/layers/routing-stats).

--- x/multilayer/client/cli/query.go
+++ x/multilayer/client/cli/query.go
@@ -4,6 +4,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 
 	"github.com/qorechain/qorechain-core/x/multilayer/types"
 )
@@ -16,6 +17,90 @@ func GetQueryCmd() *cobra.Command {
 		SuggestionsMinimumDistance: 2,
 		RunE:                       client.ValidateCmd,
 	}
+	cmd.AddCommand(cmdParams(), cmdLayer(), cmdLayers(), cmdRoutingStats())
+	return cmd
+}
 
+func cmdParams() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "params",
+		Short: "Show the multilayer module params",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			res, err := types.NewQueryClient(clientCtx).Params(cmd.Context(), &types.QueryParamsRequest{})
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+func cmdLayer() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "layer [layer-id]",
+		Short: "Show a single layer by ID",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			res, err := types.NewQueryClient(clientCtx).Layer(cmd.Context(), &types.QueryLayerRequest{LayerId: args[0]})
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+func cmdLayers() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "layers",
+		Short: "List all layers",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			res, err := types.NewQueryClient(clientCtx).Layers(cmd.Context(), &types.QueryLayersRequest{})
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+func cmdRoutingStats() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "routing-stats",
+		Short: "Show cross-layer routing statistics",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			res, err := types.NewQueryClient(clientCtx).RoutingStats(cmd.Context(), &types.QueryRoutingStatsRequest{})
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
 	return cmd
 }
